8de84b071b9702a33115d656793684911c048f56
aidostraining <60025201+aidostraining@users.noreply.github.com>
2020-02-10 21:00:58 +0600

    Order.java added
    Order.java added to the coffeehouse project

---
+++ src/Order.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by user on 10.02.2020.
+ */
+public class Order {
+    public int coffee;
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public void orderTalk() {
+            BaristaList list = new BaristaList("Hello! What would you like to order?"
+                    + "\n Don't hesitate and Enter 1 number to get best Coffee" + "\n Or if you want Chocolate Enter number 2  ");
+        System.out.println(list.listMenu);
+        try {
+            int coffeeNum = Integer.parseInt(reader.readLine());
+            if (coffeeNum == 1) {
+                System.out.println("Enter number for coffee you want to choose:");
+                System.out.println("1 for Latte \n" +
+                        "2 for Cappuccino \n" + "3 for Americano \n");
+                coffee = Integer.parseInt(reader.readLine());
+                final int LATTE_NUM = 1;
+                final int CAPPUCCINO_NUM = 2;
+                final int AMERICANO_NUM = 3;
+                switch (coffee) {
+                    case LATTE_NUM:
+                        Latte latte = new Latte();
+
+                        System.out.println(latte.getDescription() + " " + latte.price() + " tenge");
+                        System.out.println("Thank you for choosing our Coffee Hause!");
+                        break;
+
+                    case CAPPUCCINO_NUM:
+                        Cappuccino cappuccino = new Cappuccino();
+                        System.out.println(cappuccino.getDescription() + " " + cappuccino.price() + " tenge");
+                        System.out.println("Thank you for choosing our Coffee Hause!");
+                        break;
+
+                    case AMERICANO_NUM:
+                        Americano americano = new Americano();
+                        System.out.println(americano.getDescription() + " " + americano.price() + " tenge");
+                        System.out.println("Thank you for choosing our Coffee Hause!");
+                        break;
+                    default:
+                        System.out.println("Please, try input right number");
+                }
+            }
+            else if (coffeeNum == 2) {
+                Chocolate chocolate = new Chocolate();
+                System.out.println(chocolate.getDescription() + " " + chocolate.price() + " tenge");
+                System.out.println("Thank you for choosing our Coffee Hause!");
+            }
+            else {
+                System.out.println("Please, try input right number");
+            }
+        }catch (Exception exc) {
+            System.err.println("Please, try input right number");
+        }
+    }
+}
