6fa689af28cb80040f3fc95cc451fc3c9fa4d53a
aidostraining <60025201+aidostraining@users.noreply.github.com>
2020-02-11 16:01:30 +0600

    changes added
    changes added

--- src/BaristaList.java
+++ src/BaristaList.java
@@ -2,8 +2,8 @@
  * Created by user on 04.02.2020.
  */
 public class BaristaList {
-    public  String listMenu = "" ;
-    public BaristaList(String listMenu) {
-        this.listMenu = listMenu;
+    public  String getMenu = "" ;
+    public BaristaList(String getMenu) {
+        this.getMenu = getMenu;
     }
 }
