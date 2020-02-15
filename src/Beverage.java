1bc6ba39ef396b9034d24fd2abdf519b50205ce6
aidostraining <60025201+aidostraining@users.noreply.github.com>
2020-02-15 08:47:29 +0600

    minor changes added

--- src/Beverage.java
+++ src/Beverage.java
@@ -2,9 +2,9 @@
  * Created by user on 04.02.2020.
  */
 public abstract class Beverage {
-    public String description = "Beverage";
-    public String getDescription() {
-        return description;
+    private String coffeeName = "Beverage";
+    public String getCoffeeName() {
+        return coffeeName;
     }
     public abstract int price();
 }
