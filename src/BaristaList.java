a304533d21a3cab9a2efea2a3c453334fe734c0d
aidostraining <60025201+aidostraining@users.noreply.github.com>
2020-02-15 08:48:08 +0600

    minor chnges added

--- src/BaristaList.java
+++ src/BaristaList.java
@@ -2,8 +2,9 @@
  * Created by user on 04.02.2020.
  */
 public class BaristaList {
-    public  String listMenu = "" ;
-    public BaristaList(String listMenu) {
-        this.listMenu = listMenu;
+    public void baristaGreeting(){
+        System.out.println("Hello! What would you like to order?"
+                + "\n Don't hesitate and Enter 1 number to get best Coffee" +
+                "\n Or if you want Chocolate Enter number 2  ");
     }
 }
